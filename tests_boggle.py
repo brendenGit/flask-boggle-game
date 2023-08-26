742243a94ad701cb7ccaa320349b65d00c99925c
brenden <brendenjarias@gmail.com>
2023-08-26 11:25:29 -0700

    updated tests for tests_boggle.py

--- tests_boggle.py
+++ tests_boggle.py
@@ -6,12 +6,16 @@ class BoggleTests(TestCase):
     def setUp(self):
         self.boggle = Boggle()
 
+
+
     def test_read_dict(self):
         words = self.boggle.read_dict("words.txt")
         self.assertIsInstance(words, list)
         self.assertGreater(len(words), 0)
         for word in words:
             self.assertTrue(isinstance(word, str))
+
+
 
     def test_make_board(self):
         board = self.boggle.make_board()
@@ -22,18 +26,28 @@ class BoggleTests(TestCase):
             for letter in row:
                 self.assertTrue(letter.isalpha())
 
-    def test_check_valid_word(self):
-        valid_word = "TEST"
-        invalid_word = "XYZ"
-        not_a_word = "HJKL"
 
-        result_valid_word = self.boggle.check_valid_word(self.boggle.make_board(), valid_word)
-        result_invalid_word = self.boggle.check_valid_word(self.boggle.make_board(), invalid_word)
-        result_not_a_word = self.boggle.check_valid_word(self.boggle.make_board(), not_a_word)
+
+    def test_check_valid_word(self):
+        board = [['T', 'E', 'S', 'T', 'T'],
+                 ['T', 'E', 'S', 'T', 'T'],
+                 ['T', 'E', 'S', 'T', 'T'],
+                 ['T', 'E', 'S', 'T', 'T'],
+                 ['T', 'E', 'S', 'T', 'T']]
+        
+        valid_word = "test"
+        invalid_word = "guess"
+        not_a_word = "hjkl"
+
+        result_valid_word = self.boggle.check_valid_word(board, valid_word)
+        result_invalid_word = self.boggle.check_valid_word(board, invalid_word)
+        result_not_a_word = self.boggle.check_valid_word(board, not_a_word)
 
         self.assertEqual(result_valid_word, "ok")
         self.assertEqual(result_invalid_word, "not-on-board")
         self.assertEqual(result_not_a_word, "not-word")
+
+
 
     def test_find(self):
         board = [['T', 'E', 'S', 'T', 'T'],
